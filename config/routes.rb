66872b6b79b82e329cfc897d861182f64e506f03
Gidayu Samala Lendra <gidayu.samala@gmail.com>
2016-07-14 14:38:27 +0700

    Modify home view, profile view, contact view, routes, application view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'contacts/index'
+
+  get 'profiles/index'
+
+  get 'homes/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'homes#index'
 end
